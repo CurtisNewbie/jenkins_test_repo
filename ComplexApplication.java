2c290f9c9278621736affc569596bf7b67d0c563
zhuangyongjie <yongjie.zhuang@swiftpass.cn>
2020-08-23 17:46:27 +0800

    Fix

--- ComplexApplication.java
+++ ComplexApplication.java
@@ -2,8 +2,9 @@ public class ComplexApplication {
 
     public static void main(String[] args){
         System.out.println("Started very complex application");
-        System.out.printf("Executing %d task\n", i);
-        for(int i = 0; i < 3; i++){
+        int tasks = 3;
+        System.out.printf("Executing %d task\n", tasks);
+        for(int i = 0; i < tasks; i++){
             System.out.println("Task " + i);
             try{
                 Thread.sleep(500);
